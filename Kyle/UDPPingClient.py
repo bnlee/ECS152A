6c14b5f60a2f776cbb043a23718f19801c246032
kjtanis <kjtanis@ucdavis.edu>
2013-10-21 23:39:00 -0700

    Fixed some formatting with time and rounding errors

--- Kyle/UDPPingClient.py
+++ Kyle/UDPPingClient.py
@@ -1,17 +1,25 @@
 import random
 from socket import *
 from datetime import *
+import time
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.connect(('', 12000))
 clientSocket.settimeout(1)
-for x in range(1, 10):
-  message = 'Ping '+str(x)+' '+date.today().isoformat()+' T '
-  message += datetime.now().time().isoformat()+' UTC'
-  print message
-  clientSocket.send(message)
-  message2, address = clientSocket.recvfrom(1024)
-  print message2
-  print 'RTT: \n'
+for x in range(1, 11):
+    message = 'Ping '+str(x)+' '+date.today().isoformat()+' T '
+    message += str(datetime.now().minute)+':'+str(datetime.now().second)+' UTC'
+    #print 'Sending to Server: \'{0}\''.format(message)
+    print message
+    sent_time = time.time() * 1000
+    try:
+        clientSocket.send(message)
+        message2, address = clientSocket.recvfrom(1024)
+        recv_time = time.time() * 1000
+        #print 'Received from Server: \'{0}\''.format(message2)
+        print message2
+        print 'RTT: {0}\n'.format(round(recv_time-sent_time, 3))
+    except timeout:
+        print 'Request timed out\n'
 clientSocket.close()
 
